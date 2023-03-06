42f6db0315b58cb34a338a2f98b265eaf8d1789f
mitchellposk <mitchposk@gmail.com>
2023-03-06 16:54:31 -0500

    add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useSession } from 'next-auth/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const { data: session } = useSession()
+  console.log({ session })
   return (
     <>
       <Head>
@@ -14,9 +15,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          yoo
+      <main className='text-center'>
+        <div>
+          <h1 className='text-slate-200 text-3xl mt-12'>Demo</h1>
+          <h3 className='text-slate-400 text-xl mt-6'>Welcome, {session?.user?.name}</h3>
         </div>
       </main>
     </>
