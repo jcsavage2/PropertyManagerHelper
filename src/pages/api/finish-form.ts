bc0d91c64825a83729332f0d3fdf8f0395e26e47
mitchellposk <mitchposk@gmail.com>
2023-03-18 19:34:14 -0400

    prompt refinements

--- src/pages/api/finish-form.ts
+++ src/pages/api/finish-form.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
 import { ChatCompletionRequestMessage, Configuration, CreateChatCompletionResponse, OpenAIApi } from "openai"
-import { FinishFormRequest } from "../new-request"
+import { FinishFormRequest } from "../"
 
 type Data = {
   response: string
@@ -9,14 +9,15 @@ type Data = {
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const body = req.body as FinishFormRequest
-  const { text, messages, workOrder } = body
+  const { userMessage, messages, ...workOrder } = body
 
   const config = new Configuration({
     apiKey: process.env.OPEN_AI_API_KEY,
   })
   const openai = new OpenAIApi(config)
 
-  const prompt: ChatCompletionRequestMessage = {"role": "system", "content": `CONTEXT: examine this JSON: ${JSON.stringify(workOrder)}
+  const prompt: ChatCompletionRequestMessage = {
+    "role": "system", "content": `CONTEXT: examine this JSON: ${JSON.stringify(workOrder)}
   As a property management chatbot provided with an issue from the user, your job is to update the JSON fields based on the users \
   input and RETURN the updated JSON back in a readable form. For example, User: "Dylan apt12c. Yes." \
   You: "Name: Dylan, Address: Apt 12c, Permission to enter(y/n): Yes, Service Request: Toilet; Clogged." If the user's input is totally \
@@ -26,7 +27,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   const response = await openai.createChatCompletion({
     max_tokens: 128,
     model: "gpt-3.5-turbo",
-    messages: [prompt, ...messages, { role: "user", content: text }],
+    messages: [prompt, ...messages, { role: "user", content: userMessage }],
     temperature: 0,
   })
 
