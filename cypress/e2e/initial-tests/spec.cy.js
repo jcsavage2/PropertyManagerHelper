35c9b9efc6ea4289144e969d0a7d73e9e81d98c1
mitchellposk <mitchposk@gmail.com>
2023-03-29 13:03:48 -0400

    Update cypress tests

--- cypress/e2e/initial-tests/spec.cy.js
+++ cypress/e2e/initial-tests/spec.cy.js
@@ -16,13 +16,13 @@ describe('Go to Home Page', () => {
   it('Pro user identifies issue in one response', () => {
     cy.get('[data-testid="userMessageInput"]').type("Toilet is leaking from the tank, and the toilet is located in the upstairs bathroom on the right.")
     cy.get('[data-testid="send"]').click()
-    cy.wait(7000)
+    cy.wait(5000)
     cy.get('[data-testid="response-1"]').should("contain", `Please complete the form below. When complete, and you have given permission to enter, click the "submit" button to send your Service Request.`)
   })
-  // it('Noob user gets a follow up question', () => {
-    // cy.get('[data-testid="userMessageInput"]').type("Toilet is broken.")
-    // cy.get('[data-testid="send"]').click()
-    // cy.wait(7000)
-    // cy.('[data-testid="final-response"]').should("not.contain", `Please complete the form below. When complete, and you have given permission to enter, click the "submit" button to send your Service Request.`)
-  // })
+  it('Noob user gets a follow up question', () => {
+    cy.get('[data-testid="userMessageInput"]').type("Toilet is broken.")
+    cy.get('[data-testid="send"]').click()
+    cy.wait(5000)
+    cy.get('[data-testid="response-1"]').should("not.contain", `Please complete the form below. When complete, and you have given permission to enter, click the "submit" button to send your Service Request.`)
+  })
 })
