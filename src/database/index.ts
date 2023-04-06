b389a1d148379ef12315436570c52d4ba1a7f746
mitchellposk <mitchposk@gmail.com>
2023-04-05 23:29:59 -0400

    get email auth working

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,42 +4,29 @@ import { Table } from 'dynamodb-toolbox';
 
 const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-const region = process.env.AWS_REGION;
+const region = process.env.REGION;
+
 if (!accessKeyId || !secretAccessKey || !region) {
   throw new Error("missing aws credentials");
 }
 
 export const DynamoDBClientConfig = {
-    credentials: {
-        accessKeyId: accessKeyId,
-        secretAccessKey: secretAccessKey,
-    },
-    region: process.env.AWS_REGION,
-}
+  credentials: {
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+  },
+  region: process.env.REGION,
+};
 
-export const DocumentClient = new DynamoDB.DocumentClient({...DynamoDBClientConfig, convertEmptyValues: false});
+export const DocumentClient = new DynamoDB.DocumentClient({ ...DynamoDBClientConfig, convertEmptyValues: false });
 
 export const PillarDynamoTable = new Table({
-  //DynamoDB Table Name
   name: 'pillar-hq',
-
-  // Define partition and sort keys
   partitionKey: 'pk',
   sortKey: 'sk',
-  
   removeNullAttributes: true,
-  DocumentClient // Add the DocumentClient
-});
-
-const NextAuthTable = new Table({
-  name: "next-auth",
-  partitionKey: "pk",
-  sortKey: "sk",
-  indexes: {
-    GSI1: { partitionKey: "GSI1PK", sortKey: "GSI1SK" },
-  },
   DocumentClient
-})
+});
 
 export class BaseEntity {
   pk: string;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -8,51 +8,36 @@ import { DynamoDBDocument } from "@aws-sdk/lib-dynamodb";
 
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-const region = process.env.AWS_REGION;
+const region = process.env.REGION;
+
+const emailHost = 'smtp.sendgrid.net';
+const emailUsername = 'apikey'; // <- don't replace "apikey" it's the actual username
+const smtpPort = 'apikey'; // <- don't replace "apikey" it's the actual username
+const emailPassword = process.env.SMTP_PASSWORD;
+
 if (!clientId || !clientSecret || !region) {
     throw new Error('Missing Auth Credentials!');
 }
 
 const client = DynamoDBDocument.from(new DynamoDB(DynamoDBClientConfig), {
-  marshallOptions: {
-    convertEmptyValues: true,
-    removeUndefinedValues: true,
-    convertClassInstanceToMap: true,
-  },
-})
+    marshallOptions: {
+        convertEmptyValues: true,
+        removeUndefinedValues: true,
+        convertClassInstanceToMap: true,
+    },
+});
 
 export default NextAuth({
     providers: [
+        Email({
+            server: `smtp://${emailUsername}:${emailPassword}@${emailHost}:${smtpPort}`,
+            from: 'dylan@pillarhq.co',
+        }),
         GoogleProvider({
             clientId,
             clientSecret,
         }),
-        Email({
-            server: {
-                host: process.env.SMTP_HOST,
-                port: Number(process.env.SMTP_PORT),
-                auth: {
-                    user: process.env.SMTP_USER,
-                    pass: process.env.SMTP_PASSWORD,
-                },
-            },
-            from: 'mitchposk@gmail.com', 
-        }),
     ],
     adapter: DynamoDBAdapter(client),
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            return true;
-        },
-        async redirect({ url, baseUrl }) {
-            return baseUrl;
-        },
-        async session({ session, user, token }) {
-            return session;
-        },
-        async jwt({ token, user, account, profile, isNewUser }) {
-            return token;
-        },
-    },
     secret: process.env.JWT_SECRET,
 });
