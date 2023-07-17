c99d8ac5200999491e69185b90d239d8a8a235bc
mitchellposk <mitchposk@gmail.com>
2023-07-16 20:00:41 -0400

    in progress banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,54 @@
 import { useUserContext } from "@/context/user";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Modal from 'react-modal';
 
 const Home = () => {
   const router = useRouter();
   const { query } = router;
   const { login, sessionUser } = useUserContext();
+  const [showNotice, setShowNotice] = useState(false);
 
   if (query?.authredirect && !sessionUser) {
     signIn();
   }
 
+  useEffect(() => {
+    const hasSeenNotice = sessionStorage.getItem("PILLAR::NOTICE");
+    if (hasSeenNotice) {
+      return;
+    }
+    else {
+      setShowNotice(true);
+      sessionStorage.setItem("PILLAR::NOTICE", "false");
+    };
+  }, []);
+
+  function closeModal() {
+    setShowNotice(false);
+  }
+
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+      width: "75%",
+      backgroundColor: 'rgba(255, 255, 255)'
+    },
+    overLay: {
+      position: 'fixed',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: 'rgba(25, 255, 255, 0.75)'
+    }
+  };
 
   return (
     <>
@@ -49,6 +87,27 @@ const Home = () => {
               }}>Continue as Technician</button>
           </div>
         )}
+        <Modal
+          isOpen={showNotice}
+          onAfterOpen={() => { }}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+          closeTimeoutMS={200}
+          style={customStyles}
+        >
+          <div className="w-full text-right">
+            <button
+              className="bg-blue-200 px-2 py-1 text-gray-600 hover:bg-blue-300 rounded disabled:opacity-25"
+              onClick={closeModal}>
+              X Close
+            </button>
+          </div>
+          <div className="w-full text-center">
+            <h3 className="mt-5">NOTICE</h3>
+            <p>This application is in the process of being developed and is not yet stable for public use. Please contact dylan@pillarhq.co if you would like more information.</p>
+          </div>
+
+        </Modal>
       </div>
     </>
   );
