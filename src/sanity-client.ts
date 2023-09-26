b9a74c43617ea1a0258ffe808cdcbef0f364d9e8
mitchellposk <mitchposk@gmail.com>
2023-09-26 14:25:15 -0400

    remove sanity

--- src/sanity-client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity"
-
-export const client = createClient({
-  projectId: "s5ypecxw",
-  dataset: "production",
-  apiVersion: "2022-03-25",
-  useCdn: true
-})
