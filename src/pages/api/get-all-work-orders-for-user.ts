9425657a72df28a2c7171873491d8f4b04c93adb
mitchellposk <mitchposk@gmail.com>
2023-09-27 15:38:15 -0400

    Add sentry

--- src/pages/api/get-all-work-orders-for-user.ts
+++ src/pages/api/get-all-work-orders-for-user.ts
@@ -22,7 +22,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
     return res.status(200).json({ response: JSON.stringify({ workOrders, startKey: response.startKey }) });
   } catch (error) {
-    console.log({ error });
     return res.status(500).json({ response: '' });
   }
 }
