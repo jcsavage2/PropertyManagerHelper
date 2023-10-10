a0fac7c79e13a7fdcd3f87cab7ac09433513b452
mitchellposk <mitchposk@gmail.com>
2023-10-10 17:04:46 -0400

    Add error message on adding wo modal

--- src/components/add-work-order-modal.tsx
+++ src/components/add-work-order-modal.tsx
@@ -145,7 +145,7 @@ export const AddWorkOrderModal = ({
         setShowAdditionalOptions(false);
       } catch (err) {
         console.log({ err });
-        toast.error('Error Submitting Work Order. Please Try Again', {
+        toast.error((err as any)?.response?.data?.response ?? 'Error Submitting Work Order. Please Try Again', {
           position: toast.POSITION.TOP_CENTER,
           draggable: false,
         });
