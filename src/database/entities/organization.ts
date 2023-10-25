be1d17edd802c587c26e9066ee17e82cdc442acf
mitchellposk <mitchposk@gmail.com>
2023-10-25 18:48:25 -0400

    update logic to create org

--- src/database/entities/organization.ts
+++ src/database/entities/organization.ts
@@ -42,11 +42,10 @@ export class OrganizationEntity {
   });
 
   public async create({ name, uuid }: CreateOrgProps) {
-    const orgId = generateKey(ENTITY_KEY.ORGANIZATION, uuid);
     const result = await this.organizationEntity.update(
       {
-        pk: orgId,
-        sk: orgId,
+        pk: uuid,
+        sk: uuid,
         name,
       },
       { returnValues: 'ALL_NEW', strictSchemaCheck: true }
@@ -54,7 +53,7 @@ export class OrganizationEntity {
     return result.Attributes;
   }
 
-  public async delete({ pk, sk }: { pk: string; sk: string }) {
+  public async delete({ pk, sk }: { pk: string; sk: string; }) {
     const params = {
       pk,
       sk,
